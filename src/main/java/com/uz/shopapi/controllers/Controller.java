fbd3a0920ebee1e1e4a3124aa53a8bffb8717e0f
Turgun <hujaev@list.ru>
2021-04-22 09:36:14 +0500

    Revert "slavemain da hato bor . intellej ga git ulandi"
    This reverts commit 29ce553396061802c6c4a2ab1d109f371dc3fbf0.

--- src/main/java/com/uz/shopapi/controllers/Controller.java
+++ src/main/java/com/uz/shopapi/controllers/Controller.java
@@ -209,7 +209,7 @@ public class Controller {
     public ResponseEntity<Integer> addMainSlave(@RequestBody RequestMainSlave requestMainSlave) {
         return ResponseEntity.ok(mainService.addMainSlave(requestMainSlave));
     }
-    //TEKSHIRALIZ GITHUBNI
+
     @DeleteMapping(path = "/delMainSlave/{id}")
     public ResponseEntity<Integer> deleteMainSlave(@PathVariable("id") Integer id) throws Exception {
         return ResponseEntity.ok(mainService.deleteMainSlave(id));
